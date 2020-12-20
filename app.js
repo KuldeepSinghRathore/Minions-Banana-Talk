e792847cff7c57468ff6d906cdc037415c16f513
kuldeep singh <kuldeeprathore74@gmail.com>
2020-12-20 17:00:38 +0530

    all functionality is done

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ var txtinput = document.querySelector('#txt-input');
 // console.log(txtinput);
 var outputDiv = document.querySelector("#output");
 
+// var mockApiUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var mockApiUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return mockApiUrl + "?" + "text=" +text
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("There is issue with server please try after Sometimes")
+}
+
 function clickEventHandler() {
     
-     outputDiv.innerText = "lalalaljjsjjs   " + txtinput.value;
+  var  inputText = txtinput.value;
+
+  //calling fetch
+  fetch(getTranslationURL(inputText))
+   .then(function responseHandler(response){
+       return response.json()
+   })
+   .then(function logJSON(json){
+       translatedText = json.contents.translated;
+       outputDiv.innerText = translatedText; 
+   })
+   .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickEventHandler );
